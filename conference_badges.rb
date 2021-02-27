6df488ec66cc56afd15a1ab1895229231734d685
Mick Nelson <mickbbnelson@gmail.com>
2021-02-27 10:56:27 -0800

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.collect do |name|
+    "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    new_array = []
+    names.each.with_index(1) do |name, index|       #The with_index method takes an optional parameter to offset the starting index. each_with_index does the same thing, but has no optional starting index.
+        new_array << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    return new_array
+end
+
+def printer(names)
+    batch_badge_creator(names)
+        names.each do |name|
+            puts "Hello, my name is #{name}."
+        end
+    assign_rooms(names)
+        room = 1
+        names.each do |name|
+                puts "Hello, #{name}! You'll be assigned to room #{room}!"
+                room += 1
+        end
+end
